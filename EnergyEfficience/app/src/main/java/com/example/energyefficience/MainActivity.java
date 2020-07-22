75dfbade667f7cc6d5ebc2e10daafd11a60a90dd
Niklas Kluge <niklaskluge96@gmail.com>
2020-07-23 01:12:13 +0200

    first real commit

---
+++ EnergyEfficience/app/src/main/java/com/example/energyefficience/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.energyefficience;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+    MyFragmentStateAdapter adapter;
+    ViewPager2 viewpager;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.addTab(tabLayout.newTab());
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        viewpager = findViewById(R.id.pager);
+        adapter = new MyFragmentStateAdapter(this, tabLayout.getTabCount());
+        viewpager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewpager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("Base64");
+                        break;
+                    case 1:
+                        tab.setText("Sorting");
+                        break;
+                    default: break;
+                }
+            }
+        }).attach();
+    }
+}
